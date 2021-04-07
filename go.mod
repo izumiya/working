c4ec126ca331f778c53e5cb2f70be1fdb533effd
izumiya <92346+izumiya@users.noreply.github.com>
2021-04-08 00:31:53 +0900

    feat: implement POST and PUT methods

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/izumiya/working
 
 go 1.16
+
+require github.com/nicholasjackson/env v0.6.0
--- main.go
+++ main.go
@@ -10,31 +10,42 @@ import (
 	"time"
 
 	"github.com/izumiya/working/product-api/handlers"
+	"github.com/nicholasjackson/env"
 )
 
-func main() {
-	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
 
+func main() {
+	env.Parse()
+
+	l := log.New(os.Stdout, "product-api ", log.LstdFlags)
+
+	// create the handlers
 	ph := handlers.NewProducts(l)
 
+	// create a new serve mux and register the handlers
 	sm := http.NewServeMux()
 	sm.Handle("/", ph)
 
+	// create a new server
 	s := &http.Server{
-		Addr:         ":9090",
-		Handler:      sm,
-		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the looger for the server
+		ReadTimeout:  1 * time.Second,   // max time to read request from the client
+		WriteTimeout: 1 * time.Second,   // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connection using TCP Keep-Alive
 	}
 
+	// start the server
 	go func() {
+		l.Println("Starting server on port 9090")
+
 		err := s.ListenAndServe()
 		if err != nil {
 			l.Fatal(err)
 		}
 	}()
-	l.Println("Start")
 
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
